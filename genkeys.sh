b6292386a9804f404b1fe56017796c795636bc3d
kxknet <serviceseo@gmail.com>
2016-04-08 11:39:50 +0300

    add missing lsb tags

--- genkeys.sh
+++ genkeys.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+### BEGIN INIT INFO
+# Provides: ssh gen keys
+# Required-Start: $remote_fs $syslog
+# Required-Stop: $remote_fs $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: ssh gen keys
+# Description: Enable
 if test ! -e /etc/ssh/lock_gen_keys ; then
 rm -f /etc/ssh/ssh_host_*
 ssh-keygen -A
